98df973a05b064e57414d1252b888f5e6f6de801
arthurhernandezp <arthurhernandezp>
2024-09-18 19:38:42 -0300

    Adicionando Diretorio de conteudo de I/O

--- 1.FirstSteps/1.2_InputOutputData/main.cpp
+++ 1.FirstSteps/1.2_InputOutputData/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-
-consteval int get_value(){
-    return 3;
-}
+#include <string>//Necessario para usar getline()
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    /*
+    int x {4};
+    int y {2};
+    int soma = x + y;
+    std::cout << soma << '\n';
+    // Pegar Dois paramentos diferenciando eles com espaço ex: 2 4 
+    std::cin >> x >> y;
+    soma = x + y;
+    std::cout << soma << '\n';
+
+    //std::cin 
+    std::string palavra{};
+    std::cout << palavra << '\n';
+
+    std::cout << "Digite uma palavra: ";
+    std::cin >> palavra;
+    std::cout << palavra << '\n';
+    
+    //std::getline
+
+    std:: string frase{};
+    std::cout << frase << '\n';
+    std::getline(std::cin,frase);
+    std::cout << frase << '\n';
+
+    //std::cerr
+    std::cerr << "Mensagem de erro!!!" << '\n';
+
+    //std::clog
+    std::clog << "Mensagem de log!!!" << '\n';
+    */
     return 0;
 }
