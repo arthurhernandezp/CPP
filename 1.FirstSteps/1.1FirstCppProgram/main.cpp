59936be4fab0193bbaeeb3f599ec83cf7a657c29
arthurhernandezp <arthurhernandezp>
2024-09-18 19:17:35 -0300

    Adicionando Capitulo dos Primeiros passos

---
+++ 1.FirstSteps/1.1FirstCppProgram/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+consteval int get_value(){
+    return 3;
+}
+
+int main(){
+    std::cout << "Estudando C++!\n";
+    std::cout << "Testando acentos ´`~~^^çç \n";
+
+    return 0;
+}
