e12a705f668e1f1fe6121f009d903df0435b5017
arthurhernandezp <arthurhernandezp>
2024-09-20 04:29:30 -0300

    Adicionando diretorio de "Auto"

---
+++ 2.VariablesAndDataTypes/2.7_Auto/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    auto var1 {12}; //int
+    auto var2 {13.0}; //double
+    auto var3 {14.0f}; //float
+    auto var4 {15.0l}; //long
+    auto var5 {'e'}; // char
+
+    //Modificações do tipo de inteiro 
+    auto var6 {44u}; // unsigned int
+    auto var7 {123ul}; // unsigned long
+    auto var8 {123ll}; //long long
+    
+    return 0;
+}
