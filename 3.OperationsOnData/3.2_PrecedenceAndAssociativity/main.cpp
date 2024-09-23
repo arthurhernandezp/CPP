60f8b43371743aa28218e159e25697efe9c634c1
arthurhernandezp <arthurhernandezp>
2024-09-23 00:52:47 -0300

    Adicionando precendencia e associatividade de operações

---
+++ 3.OperationsOnData/3.2_PrecedenceAndAssociativity/main.cpp
@@ -0,0 +1,18 @@
+//https://en.cppreference.com/w/cpp/language/operator_precedence
+#include <iostream>
+
+int main(){
+
+    int a{6};
+    int b{3};
+    int c{8};
+    int d{9};
+    int e{3};
+    int f{2};
+    int g{5};
+
+    int result = a + b * c - d/e - f + g;
+    std::cout << "Resultado e: " << result << '\n';
+
+    return 0;
+}
