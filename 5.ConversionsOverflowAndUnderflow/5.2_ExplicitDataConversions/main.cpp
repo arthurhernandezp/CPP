43d99d2079e0822aaa5a14e65b60d3c71618192c
arthurhernandezp <arthurhernandezp>
2024-09-30 15:00:41 -0300

    Explicit conversions usando static_cast<>()

--- 5.ConversionsOverflowAndUnderflow/5.2_ExplicitDataConversions/main.cpp
+++ 5.ConversionsOverflowAndUnderflow/5.2_ExplicitDataConversions/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-consteval int get_value(){
-    return 3;
-}
-
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    
+    // Explicit conversion
+    // static_cast<tipofinal>(tipoinicial)
+
+    double n1 {54.3};
+    double n2 {43.7};
+
+    int sum = static_cast<int>(n1) + static_cast<int>(n2);
+    std::cout << "int sum = static_cast<int>(n1) + static_cast<int>(n2) : " << sum << '\n';
+    sum = static_cast<int>(n1 + n2);
+    std::cout << "sum = static_cast<int>(n1 + n2) : " << sum << '\n';
+
     return 0;
 }
+
