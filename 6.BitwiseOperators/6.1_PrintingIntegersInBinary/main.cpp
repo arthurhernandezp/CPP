59c33eaa0b808e8188553349d51ba28c7c237372
arthurhernandezp <arthurhernandezp>
2024-10-01 13:04:50 -0300

    Biblioteca bitset e como mudar a base para binario

--- 6.BitwiseOperators/6.1_PrintingIntegersInBinary/main.cpp
+++ 6.BitwiseOperators/6.1_PrintingIntegersInBinary/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-
-consteval int get_value(){
-    return 3;
-}
+#include <bitset> //Usado para trabalhar com a base binaria
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    
+    unsigned short int dado {15};
+    std::cout << "dado (std::dec) : " << std::showbase << std::dec << dado << '\n';
+    std::cout << "dado (std::oct) : " << std::showbase << std::oct << dado << '\n';
+    std::cout << "dado (std::hex) : " << std::showbase << std::hex << dado << '\n';
+    std::cout << "dado (std::bin) : " << std::bitset<16>(dado) << '\n';
+
     return 0;
 }
