916c89a5797cbdf8b2d4f79c6b54a65ca1c13603
arthurhernandezp <arthurhernandezp>
2024-10-01 01:29:35 -0300

    Overflow e Underflow de variaveis

--- 5.ConversionsOverflowAndUnderflow/5.3_OverflowAndUnderflow/main.cpp
+++ 5.ConversionsOverflowAndUnderflow/5.3_OverflowAndUnderflow/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 
-consteval int get_value(){
-    return 3;
-}
-
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    
+    //Overflow
+
+    unsigned char data {250}; // Uso de char para ter uma variavel de 1 byte ( 0 ate 255 )
+
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //251
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //252
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //253
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //254
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //255
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //0
+    std::cout << "Data: " << static_cast<int>(++data) << '\n';      //1
+    std::cout << '\n';
+
+    //Underflow
+    data = 2;
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //1
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //0
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //255
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //254
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //253
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //252
+    std::cout << "Data: " << static_cast<int>(--data) << '\n';      //251
+    
     return 0;
 }
