5a2585558265554c21736d185b494496559d5398
arthurhernandezp <arthurhernandezp>
2024-12-07 22:32:02 -0300

    Adicionado Exemplo de transformação 2D : Rotação e Escala

---
+++ OpenGL/Transformacoes_2D_ROTACAO/Transformacoes_2D/main.cpp
@@ -0,0 +1,62 @@
+#include "Windows.h"
+#include <iostream>
+#include "GL/glut.h"
+
+void init(void);
+void display(void);
+void desenhaCasa(void);
+
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(200, 0);
+	glutInitWindowSize(400, 300);
+	glutCreateWindow("Escala com ponto fixo");
+
+	init();
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+
+	return 0;
+}
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);		// Define a cor de fundo
+
+	glMatrixMode(GL_PROJECTION);			// Carrega a matriz de projeção
+	gluOrtho2D(0, 200, 0, 150);				// Define projeção ortogonal 2D que mapeia objetos da coordenada do mundo para coordenadas da tela
+
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();				//Só será feita está transformação uma vez 
+
+	glTranslatef(50, 0, 0);			//Ponto Fixo
+	glTranslatef(100, 50, 0);
+	glRotatef(45, 0, 0, 1);
+	glTranslatef(-100, -50, 0);
+	
+	desenhaCasa();
+
+	glFlush();
+}
+
+void desenhaCasa(void)
+{
+	glBegin(GL_POLYGON);
+		glVertex2f(110, 50);
+		glVertex2f(110, 70);
+		glVertex2f(100, 80);
+		glVertex2f(90, 70);
+		glVertex2f(90, 50);
+	glEnd();
+
+}
